6ea5e45abd35362e3a9696315315875d27514a8d
ASAC876 <nu_shasta33@yahoo.co.jp>
2026-02-22 22:37:52 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'focus-v1';
+const ASSETS = [
+  './todo-app.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
